47822b1204d480dec919f7b3b07ba00a8b3498b5
Vojvodic1 <vojvodic.nikola@gmail.com>
2021-10-03 21:14:21 +0200

    Eyesee exam

---
+++ eyesee.spec.js
@@ -0,0 +1,58 @@
+///  <reference types="cypress" />
+
+describe('Eyesee Cypress Test', () =>{
+    it('Goes to Assigned URL', () =>{
+        cy.visit('https://trello.com/en/login')
+    })
+
+    it('Can not login',() =>{
+        cy.get('.bottom-form-link > :nth-child(1) > .forgotLink')
+        .click()
+
+        cy.get('.form-field')
+        .type('vaskevojvodic@gmail.com')
+
+        cy.get('#submit')
+        .click()
+
+        cy.get('#bottom-form-return-link')
+        .click()
+
+    })   
+
+    it('Login',() =>{
+        cy.get('#user')
+        .click()
+        .type('vaskevojvodic@gmail.com')
+
+        cy.get('#login')
+        .click()
+        .wait(3000)
+
+        cy.get('#password')
+        .click()
+        .type('nikola12345')
+
+        cy.get('#login-submit > .css-19r5em7')
+        .click()
+    })
+        
+    // Cypress detected a cross origin error happened on page load:
+
+    // it('Login with Google', () =>{
+    //     cy.visit('https://trello.com/en/login')
+    //     cy.get('#googleButton > .label')
+    //     .click()
+
+    //     cy.get('input[type="email"]')
+    //     .type('vaskevojvodic@gmail.com')
+    //     .get('#identifierNext button')
+    //     .click()
+    //     .wait(3000)
+        
+
+    //     cy.get('#google-auth-button > .css-19r5em7 > span')
+    //     .click()
+    // })
+
+})
